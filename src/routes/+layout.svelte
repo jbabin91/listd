7e847e88d35e226afc010b2d5b8f034729167652
Jace Babin <jbabin91@gmail.com>
2023-01-21 20:56:07 -0700

    add tailwind and skeleton for styling
    Signed-off-by: Jace Babin <jbabin91@gmail.com>

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { AppBar, AppShell, LightSwitch } from '@skeletonlabs/skeleton';
+  import '@skeletonlabs/skeleton/styles/all.css';
+  import '@skeletonlabs/skeleton/themes/theme-crimson.css';
+  import '../app.postcss';
+</script>
+
+<AppShell>
+  <svelte:fragment slot="header">
+    <AppBar>
+      <svelte:fragment slot="lead"><a href="/">listd.tv</a></svelte:fragment>
+      <svelte:fragment slot="trail"><LightSwitch /></svelte:fragment>
+    </AppBar>
+  </svelte:fragment>
+  <!-- <svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment> -->
+  <!-- <svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment> -->
+  <!-- <svelte:fragment slot="pageHeader">Page Header</svelte:fragment> -->
+  <!-- Router Slot -->
+  <!-- TODO: use variable for container width -->
+  <div class="pt-2 max-w-sm w-80 mx-auto">
+    <slot />
+  </div>
+  <!-- ---- / ---- -->
+  <!-- <svelte:fragment slot="pageFooter">Page Footer</svelte:fragment> -->
+  <!-- <svelte:fragment slot="footer">Footer</svelte:fragment> -->
+</AppShell>
